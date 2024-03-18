ebae48889905984e810f904e1dc2e51f0116da9c
aguirre-ivan <aguirre.ivang@gmail.com>
2024-03-18 18:55:16 -0300

    Estilo script de autogestion

--- install_g3w3.sh
+++ install_g3w3.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
+# Función para imprimir mensajes de color amarillo
+echo_yellow() {
+    YELLOW='\033[1;33m'  # Color amarillo
+    NC='\033[0m'         # Volver al color predeterminado
+    echo -e "${YELLOW}$@${NC}"
+}
+
+# Función para ejecutar comandos dentro del contenedor Apache
+exec_in_apache_container() {
+    docker exec -it $APACHE_CONTAINER bash -c "cd ${GUARANI_AUTOGESTION_DIR} && $@"
+}
+
 if [ -f ".env" ]; then
     source .env
 fi
 
-# echo "-- Clonando svn"
-# svn checkout http://colab.siu.edu.ar/svn/guarani3/nodos/$SIGLAS_INSTITUCION/3w/trunk/$G_VERSION g3w3
-# echo "-- svn clonado"
+echo_yellow "-- Configurando permisos"
+exec_in_apache_container "chown -R www-data:www-data instalacion/log/ instalacion/cache/ instalacion/temp/ instalacion/operaciones_inactivas/ src/siu/www/js/escalas/ src/siu/www/temp/"
+exec_in_apache_container "chmod 775 -R instalacion/log/ instalacion/cache/ instalacion/temp/ instalacion/operaciones_inactivas/ src/siu/www/js/escalas/ src/siu/www/temp/"
+echo_yellow "-- Permisos configurados"
 
-echo "-- Configurando permisos"
-docker exec -it $APACHE_CONTAINER bash -c "cd ${GUARANI_AUTOGESTION_DIR} && chown -R www-data:www-data instalacion/log/ instalacion/cache/ instalacion/temp/ instalacion/operaciones_inactivas/ src/siu/www/js/escalas/ src/siu/www/temp/"
-docker exec -it $APACHE_CONTAINER bash -c "cd ${GUARANI_AUTOGESTION_DIR} && chmod 775 -R instalacion/log/ instalacion/cache/ instalacion/temp/ instalacion/operaciones_inactivas/ src/siu/www/js/escalas/ src/siu/www/temp/"
-echo "-- Permisos configurados"
+echo_yellow "-- Levantando archivos de configuracion"
+exec_in_apache_container "cp ${GUARANI_AUTOGESTION_DIR}instalacion/alias_template.conf ${GUARANI_AUTOGESTION_DIR}instalacion/alias.conf"
+exec_in_apache_container "cp ${GUARANI_AUTOGESTION_DIR}instalacion/config_template.php ${GUARANI_AUTOGESTION_DIR}instalacion/config.php"
+exec_in_apache_container "cp ${GUARANI_AUTOGESTION_DIR}instalacion/login_template.php ${GUARANI_AUTOGESTION_DIR}instalacion/login.php"
+echo_yellow "-- Archivos copiados"
 
-echo "-- Levantando archivos de configuracion"
-docker exec -it $APACHE_CONTAINER bash -c "cd ${GUARANI_AUTOGESTION_DIR}instalacion && cp alias_template.conf alias.conf"
-docker exec -it $APACHE_CONTAINER bash -c "cd ${GUARANI_AUTOGESTION_DIR}instalacion && cp config_template.php config.php"
-docker exec -it $APACHE_CONTAINER bash -c "cd ${GUARANI_AUTOGESTION_DIR}instalacion && cp login_template.php login.php"
-echo "-- Archivos copiados"
+echo_yellow "-- Ejecutando 'composer install'"
+exec_in_apache_container "composer install --no-interaction --optimize-autoloader -d ${GUARANI_AUTOGESTION_DIR}"
+echo_yellow "-- 'composer install' finalizado"
 
-echo "-- Ejecutando 'composer install'"
-docker exec -it $APACHE_CONTAINER composer install --no-interaction --optimize-autoloader -d ${GUARANI_AUTOGESTION_DIR}
-echo "-- 'composer install' finalizado"
-
-echo "-- Copiando archivo config.php"
+echo_yellow "-- Copiando archivo config.php"
 docker cp config/config.php ${APACHE_CONTAINER}:${GUARANI_AUTOGESTION_DIR}instalacion/config.php
-echo "-- Archivo config.php copiado"
+echo_yellow "-- Archivo config.php copiado"
 
-echo "-- Ejecutando 'composer install'"
-docker exec -it $APACHE_CONTAINER composer install --no-interaction --optimize-autoloader -d ${GUARANI_AUTOGESTION_DIR}
-echo "-- 'composer install' finalizado"
-
-echo "-- Levantando sitio"
-docker exec -it $APACHE_CONTAINER bash -c "a2ensite alias.conf"
-docker exec -it $APACHE_CONTAINER bash -c "service apache2 reload"
-echo "-- Sitio levantado"
+echo_yellow "-- Levantando sitio"
+exec_in_apache_container "a2ensite alias.conf"
+exec_in_apache_container "service apache2 reload"
+echo_yellow "-- Sitio levantado"
